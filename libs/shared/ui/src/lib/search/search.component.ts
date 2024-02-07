f7cfe24fef863027662da125b410c201aba02980
Anna <anna.testing.git@gmail.com>
2024-02-07 22:51:19 +0200

    add initial overlay for search

--- libs/shared/ui/src/lib/search/search.component.ts
+++ libs/shared/ui/src/lib/search/search.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {OverlayModule} from '@angular/cdk/overlay';
 import { WeatherMapInputComponent } from '../input/input.component';
 
 @Component({
   selector: 'weather-map-search',
   standalone: true,
-  imports: [CommonModule, WeatherMapInputComponent],
+  imports: [CommonModule, OverlayModule, WeatherMapInputComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class WeatherMapSearchComponent {
+  data: string[] = ['item 1', 'item2', 'item3'];
+  searchedData: string[] = [];
   searchValue: string = '';
+  isOpen: boolean = false;
+
+  valueChangeHandler(): void {
+    this.searchedData = this.data.filter(data => data.includes(this.searchValue));
+
+    this.isOpen = !!this.searchedData.length;
+  }
 }
